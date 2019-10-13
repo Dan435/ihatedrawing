625378dd4033e9aa2210584cc799633340aabca8
Daniil <dan43575@gmail.com>
2019-10-13 18:32:58 +0500

    Refactoring + comments

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-var i = -1;
+/**
+ * @var i счётчик/ ID первых точек.
+ */
+var i = 0;
+/**
+ * @var j счётчик/ ID вторых точек.
+ */
+var j = 0;
 
 function setup(){
     let cnv; // Означает, что данный метод будет применяться только в пределах канвы
     localStorage.clear();
     cnv = createCanvas(1200, 900);
     background("#f7df1e");
-    cnv.mousePressed(placeFirstDot);
-    cnv.mouseReleased(placeSecondDot);
+    cnv.mousePressed(placeFirstPoint);
+    cnv.mouseReleased(placeSecondPoint);
 }
 
 class Store {   
@@ -20,38 +27,49 @@ class Store {
 
         return dots;
     }
-    
-    static addDot(firstPoint){
+    /**
+     * @function addDot - Добавляет первую/вторую точку в соответствующий JSON, в зависимости от переданных параметров.
+     * @param obj - Объект координат точки.
+     */
+    static addDot(obj){
         const CURRENT_DOTS = Store.getDots();
-        CURRENT_DOTS.push(firstPoint); 
+        CURRENT_DOTS.push(obj); 
         window.localStorage.setItem('points1', JSON.stringify(CURRENT_DOTS));
-        i++;  
     }
 }
 
-function placeFirstDot(){
+function placeFirstPoint(){
     const firstPoint = {
+        id: i,
         x1: mouseX,
         y1: mouseY
-    }
+    };
     Store.addDot(firstPoint);
     strokeWeight(8);
     point(mouseX, mouseY);
 }
 
 //Парсим джейсон, получаем линию
-function placeSecondDot(){
+function placeSecondPoint(){
+    const secondPoint = {
+        id: j,
+        x2: mouseX,
+        y2: mouseY
+    };
     point(mouseX, mouseY);
     strokeWeight(4);
     line(JSON.parse(window.localStorage.getItem('points1'))[i].x1,
     JSON.parse(window.localStorage.getItem('points1'))[i].y1, 
     mouseX, mouseY);
+    i++;  
+    j++;
 }
 
 // Очистка LocalStorage + канвы
 document.querySelector('#clearButton').addEventListener('click', (e) => {
     localStorage.clear();
-    i = -1;
+    i = 0;
+    j = 0;
     clear();
     setup();
 })
